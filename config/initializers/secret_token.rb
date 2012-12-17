4a5f747517c0568464ecc0565c16b0c79880fc89
dreamlx <dreamlinx@gmail.com>
2012-12-17 14:02:31 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Housinfo::Application.config.secret_token = '457fce9c19ec8d26662fa6295e913a68df3312996190a41bb7f8c59fa1902a50ba14eea31d290fc202bcc22722c7f24aba6cf924cd5990c2cfaad02922c04b3e'
